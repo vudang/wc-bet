a3fe10660d0d23789e2786a5524cb50f0c54e289
Tony <vungoc.dang@yum.com>
2022-11-09 14:25:08 +0700

    Updated logic bet info on list

--- phdv_bet/lib/src/pages/match/match_list_bet_screen.dart
+++ phdv_bet/lib/src/pages/match/match_list_bet_screen.dart
@@ -1,11 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:web_dashboard/src/api/api.dart';
+import 'package:web_dashboard/src/app.dart';
 import 'package:web_dashboard/src/color.dart';
 import 'package:web_dashboard/src/model/bet.dart';
 import 'package:web_dashboard/src/model/match.dart';
 import 'package:web_dashboard/src/model/odd.dart';
+import 'package:web_dashboard/src/model/team.dart';
 import 'package:web_dashboard/src/utils/day_helpers.dart';
+import 'package:web_dashboard/src/widgets/app_button.dart';
+import 'package:web_dashboard/src/widgets/app_confirm_popup.dart';
 import 'package:web_dashboard/src/widgets/app_text.dart';
+import 'package:web_dashboard/src/widgets/indicator.dart';
 import 'package:web_dashboard/src/widgets/team_flag.dart';
+
+import '../../widgets/congratulations.dart';
 
 
 class MatchListAndBetScreen extends StatelessWidget {
@@ -15,7 +26,8 @@ class MatchListAndBetScreen extends StatelessWidget {
   final bool enableHeader;
   final String? title;
   final Function(FootballMatch)? onSelected;
-  const MatchListAndBetScreen({
+  
+  MatchListAndBetScreen({
     super.key,
     this.enableHeader = false,
     this.title,
@@ -25,8 +37,13 @@ class MatchListAndBetScreen extends StatelessWidget {
     this.userBets = const [],
   });
 
+  late BuildContext _context;
+  late BetApi? _betApi;
+
   @override
   Widget build(BuildContext context) {
+    _context = context;
+    _betApi = Provider.of<AppState>(context).api?.betApi;
     if (enableHeader) {
       return Scaffold(
         appBar: AppBar(
@@ -93,6 +110,8 @@ class MatchListAndBetScreen extends StatelessWidget {
               ),
               SizedBox(height: 16),
               AppText(DateHelper.parseDateTime(input: match.localDate ?? "").toCurrentTimeZone()),
+              SizedBox(height: 16),
+              _placeBet(match),
               SizedBox(height: 5),
               _oddInfo(match)
             ],
@@ -135,5 +154,89 @@ class MatchListAndBetScreen extends StatelessWidget {
       return AppText("Missing odds", fontStyle: FontStyle.italic, color: SystemColor.GREY_LIGHT);
     }
     return AppText("${odd.desciption ?? ""}", fontStyle: FontStyle.italic, color: SystemColor.GREY_LIGHT);
+  }
+
+  Widget _placeBet(FootballMatch match) {
+    final odd = odds.firstWhere((element) => element.matchId == match.matchId && element.matchId != null, orElse: () => Odd());
+    if (odd.matchId == null) {
+      return Container();
+    }
+
+    final mybet = userBets.firstWhere((element) => element.matchId == match.matchId, orElse: (() => Bet()));
+    final hadBet = mybet.matchId != null;
+    final state = hadBet ? AppButtonState.disable : AppButtonState.normal;
+    final isChooseHome = hadBet && mybet.teamChoosed.isHome;
+    final isChooseAway = hadBet && mybet.teamChoosed.isAway;
+    final textColor = state == AppButtonState.disable ? SystemColor.BLACK : SystemColor.WHITE;
+
+    return Row(
+      children: [
+        Expanded(
+          child: AppSystemRegularButton(
+            state: state,
+            customTextColor: textColor,
+            text: "Choose ${match.homeTeamEn}",
+            customDisableColor: isChooseHome ? SystemColor.RED.withOpacity(0.2) : SystemColor.GREY_LIGHT.withOpacity(0.2),
+            onPressed: (() => _selectedOdds(TeamType.home(), odd))
+          )
+        ),
+        SizedBox(width: 20),
+        Expanded(
+          child: AppSystemRegularButton(
+            state: state,
+            customTextColor: textColor,
+            text: "Choose ${match.awayTeamEn}", 
+            customDisableColor: isChooseAway ? SystemColor.RED.withOpacity(0.2) : SystemColor.GREY_LIGHT.withOpacity(0.2),
+            onPressed: (() => _selectedOdds(TeamType.away(), odd))
+          )
+        ),
+      ],
+    );
+  }
+
+  _selectedOdds(TeamType teamType, Odd odd) {
+    final chooseTeam = (teamType.isHome ? odd.teamHome : odd.teamAway)?.toLowerCase();
+    final dialog = AppConfirmPopup(
+          title: "Are you sure with your decision?",
+          message: "When you press `BET $chooseTeam` button, you won't have second chance :D",
+          positiveButton: "BET $chooseTeam",
+          negativeButton: "Cancel",
+          onNegativePressed: () {
+            Navigator.of(_context).pop();
+          },
+          onPositivePressed: () {
+            Navigator.of(_context).pop();
+            _onPlaceBet(teamType, odd);
+          },
+        );
+        
+    showCupertinoModalPopup(
+        context: _context,
+        barrierColor: SystemColor.BLACK.withOpacity(0.8),
+        builder: (_) {
+          return dialog;
+    });
+  }
+        
+
+  _onPlaceBet(TeamType teamType, Odd odd) async {
+    Indicator.show(_context);
+    await _betApi?.placeBet(Bet(
+        amount: odd.amount,
+        choosedTeam: teamType.type,
+        userId: FirebaseAuth.instance.currentUser?.uid,
+        matchId: odd.matchId));
+    Indicator.hide(_context);
+    _showBetSuccessful();
+  }
+
+  _showBetSuccessful() {
+    showCupertinoModalPopup(
+        context: _context,
+        builder: (ctx) {
+          return Congratulations(completed: () {
+            Navigator.of(ctx).pop();
+          });
+        });
   }
 }
